67a86154916d78c04fdfe9fadeb7ce0dcf7035c6
Dan Mopsick <dmop497@gmail.com>
2017-04-18 17:43:13 -0400

    merge

--- prj/2/code/src/main/java/com/ccd/repositories/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ccd.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ccd.models.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-
-}
